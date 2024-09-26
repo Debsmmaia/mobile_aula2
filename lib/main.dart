52c2edba3abd692f33ad53bb116956bbaa928f87
Deborah_Maia <deborahmaia48@gmail.com>
2024-09-26 14:21:01 +0000

    Fluxos das paginas de carrinho, produtos, perfil e ajuda

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,153 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Shopping Control',
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = const ProfilePage();
+        break;
+      case 1:
+        page = const ProductsPage();
+        break;
+      case 2:
+        page = const CartPage();
+        break;
+      case 3:
+        page = const HelpPage();
+        break;
+      default:
+        throw UnimplementedError('No page for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: const [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.person),
+                      label: Text('Perfil'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.shopping_cart),
+                      label: Text('Produtos'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.shopping_basket),
+                      label: Text('Carrinho'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.help),
+                      label: Text('Ajuda'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Página de Perfil',
+        style: TextStyle(fontSize: 24),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    );
+  }
+}
+
+class ProductsPage extends StatelessWidget {
+  const ProductsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Página de Produtos',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Página do Carrinho',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class HelpPage extends StatelessWidget {
+  const HelpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Página de Ajuda',
+        style: TextStyle(fontSize: 24),
       ),
     );
   }
